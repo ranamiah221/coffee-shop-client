8dba98b2ced557720ff93d18c0b945f1dd560e2d
ranamiah221 <ranarasul21@gmail.com>
2024-11-11 23:17:28 +0600

    coffee shop client side setup

---
+++ src/Route/Router.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home/Home";
+
+
+const Router = createBrowserRouter([
+    {
+        path:'/',
+        element:<Home></Home>
+    }
+])
+
+export default Router;
